1f2616ca0497f2dd76b5acf5a55df381e5fb4d00
RenanSpim <renan.sinhorini@unesp.br>
2024-04-12 15:27:58 -0300

    comeco da implementacao de um botao para mudar o tema do navegador

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("oii");
+const alavancaContainer = document.querySelector("#alavanca-container");
+
+let temaStr = localStorage.getItem("tema");
+const temas = {
+    claro: () => {
+        console.log("claro");
+    },
+    escuro: () => {
+        console.log("escuro");
+    }
+};
+
+temas[temaStr]();
+
+alavancaContainer.addEventListener("click", e => {
+    temaStr = (temaStr === "claro") ? "escuro" : "claro";
+    temas[temaStr]();
+    localStorage.setItem("tema", temaStr);
+});
