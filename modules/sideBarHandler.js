b06d4d873037f6d8e84731ec8ea7783036ad8938
RenanSpim <renan.sinhorini@unesp.br>
2024-04-13 16:15:17 -0300

    cores e svg da moon trocadoas

--- modules/sideBarHandler.js
+++ modules/sideBarHandler.js
@@ -1,8 +1,17 @@
 const menuContainer = document.querySelector(".menu-container");
 const sideBar = document.querySelector(".side-bar");
 
-function toggleSideBar() {
-    sideBar.classList.toggle("show");
-}
+sideBar.style.width = (window.innerWidth * 0.3 < 200)
+    ? "calc(100vw - (var(--side-bar-padding) * 2))"
+    : "calc(300px - (var(--side-bar-padding) * 2))";
 
-menuContainer.addEventListener("click", toggleSideBar);
+window.addEventListener("resize", () => {
+    sideBar.style.width = (window.innerWidth * 0.3 < 200)
+    ? "calc(100vw - (var(--side-bar-padding) * 2))"
+    : "calc(300px - (var(--side-bar-padding) * 2))";
+});
+
+menuContainer.addEventListener("click", () => {
+    menuContainer.classList.toggle("show");
+    sideBar.classList.toggle("show");
+});
