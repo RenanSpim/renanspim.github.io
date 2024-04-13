1df088c80ea8b9d0a630c1fff92dbd97f02a9105
RenanSpim <renan.sinhorini@unesp.br>
2024-04-13 16:26:27 -0300

    deixa de mostrar o sidebar quando se clica pra fora dele

--- modules/sideBarHandler.js
+++ modules/sideBarHandler.js
@@ -1,3 +1,4 @@
+const navbar = document.querySelector(".nav");
 const menuContainer = document.querySelector(".menu-container");
 const sideBar = document.querySelector(".side-bar");
 
@@ -5,13 +6,29 @@ sideBar.style.width = (window.innerWidth * 0.3 < 200)
     ? "calc(100vw - (var(--side-bar-padding) * 2))"
     : "calc(300px - (var(--side-bar-padding) * 2))";
 
+function filhoTem(filho, elemento) {
+    let curr = filho;
+
+    while (curr !== null) {
+        if (curr === elemento) return true;
+        curr = curr.parentElement;
+    }
+
+    return false;
+}
+
 window.addEventListener("resize", () => {
     sideBar.style.width = (window.innerWidth * 0.3 < 200)
     ? "calc(100vw - (var(--side-bar-padding) * 2))"
     : "calc(300px - (var(--side-bar-padding) * 2))";
 });
 
-menuContainer.addEventListener("click", () => {
-    menuContainer.classList.toggle("show");
-    sideBar.classList.toggle("show");
+window.addEventListener("click", e => {
+    if (filhoTem(e.target, menuContainer)) {
+        menuContainer.classList.toggle("show");
+        sideBar.classList.toggle("show");
+    } else if (!filhoTem(e.target, sideBar) && !filhoTem(e.target, navbar)) {
+        menuContainer.classList.remove("show");
+        sideBar.classList.remove("show");
+    }
 });
