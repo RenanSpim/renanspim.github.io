fef98d928b35843f8dfdf78a347882c5b1c8c753
RenanSpim <renan.sinhorini@unesp.br>
2024-04-14 01:34:52 -0300

    pastas mudadas, nova pagina criada com a intencao de conter todos os projetos, adicionado atalhos para ambas as paginas no navegador

--- modules/sideBarHandler.js
+++ modules/sideBarHandler.js
@@ -2,6 +2,11 @@ const navbar = document.querySelector(".nav");
 const menuContainer = document.querySelector(".menu-container");
 const sideBar = document.querySelector(".side-bar");
 
+if (localStorage.getItem("sidebar") === "on") {
+    menuContainer.classList.add("show");
+    sideBar.classList.add("show");
+}
+
 sideBar.style.width = (window.innerWidth * 0.3 < 200)
     ? "calc(100vw - (var(--side-bar-padding) * 2))"
     : "calc(300px - (var(--side-bar-padding) * 2))";
@@ -27,8 +32,17 @@ window.addEventListener("click", e => {
     if (filhoTem(e.target, menuContainer)) {
         menuContainer.classList.toggle("show");
         sideBar.classList.toggle("show");
+        
+        localStorage.getItem("sidebar") === "on"
+            ? localStorage.setItem("sidebar", "off")
+            : localStorage.setItem("sidebar", "on");
     } else if (!filhoTem(e.target, sideBar) && !filhoTem(e.target, navbar)) {
         menuContainer.classList.remove("show");
         sideBar.classList.remove("show");
+        localStorage.setItem("sidebar", "off");
     }
 });
+
+setTimeout(() => {
+    sideBar.style.transition = "var(--transition)";
+}, 100);
