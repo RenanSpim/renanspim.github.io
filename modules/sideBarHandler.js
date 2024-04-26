7ba87db8d7f2d73d96a406be72704a4e2912562a
RenanSpim <renan.sinhorini@unesp.br>
2024-04-26 18:50:39 -0300

    removido local storage do sidebar

--- modules/sideBarHandler.js
+++ modules/sideBarHandler.js
@@ -1,10 +1,5 @@
 const menuContainer = document.querySelector(".menu-container");
 
-if (localStorage.getItem("sidebar") === "on") {
-    menuContainer.classList.add("show");
-    sideBar.classList.add("show");
-}
-
 function filhoTem(filho, elemento) {
     let curr = filho;
 
@@ -20,14 +15,9 @@ window.addEventListener("click", e => {
     if (filhoTem(e.target, menuContainer)) {
         menuContainer.classList.toggle("show");
         sideBar.classList.toggle("show");
-        
-        localStorage.getItem("sidebar") === "on"
-            ? localStorage.setItem("sidebar", "off")
-            : localStorage.setItem("sidebar", "on");
     } else if (!filhoTem(e.target, sideBar) && !filhoTem(e.target, navbar)) {
         menuContainer.classList.remove("show");
         sideBar.classList.remove("show");
-        localStorage.setItem("sidebar", "off");
     }
 });
 
